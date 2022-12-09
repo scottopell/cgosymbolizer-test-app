56fc3164cb91fb545b4b48cc2edd75cba2562eb1
Scott Opell <me@scottopell.com>
2022-12-09 16:56:14 +0000

    Disables go optimizations while building

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # With symbolizer present
-go build -o with_symbolizer
+go build -o with_symbolizer -gcflags '-N -l'
 sed -i 's/_ "github.com\/ianlancetaylor\/cgosymbolizer"/\/\/placeholder/' crash.go
 
-go build -o without_symbolizer
+go build -o without_symbolizer -gcflags '-N -l'
 
 # Restore the import
 sed -i 's/\/\/placeholder/_ "github.com\/ianlancetaylor\/cgosymbolizer"/' crash.go
